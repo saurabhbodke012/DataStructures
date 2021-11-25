5ac76a9b6851071eb9b97292b8a7e44f12fd10da
Saurabh Bodke <saurabhbodke@Saurabhs-MacBook-Pro.local>
2021-11-25 14:48:21 -0500

    findNumbers

--- DSA/src/Arrays/Introduction.java
+++ DSA/src/Arrays/Introduction.java
@@ -15,10 +15,33 @@ public class Introduction {
         return Math.max(count2,count1);
     }
 
-    public static void main(String[] args) {
-        int[] nums ={1,0,1,1,0,1};
+    public static int findNumbers(int[] nums) {
+        int count=0;
+        int rem=0;
+        int c=0;
 
-        System.out.println(findMaxConsecutiveOnes(nums));
+        for(int i=0; i<nums.length; i++){
+            count=0;
+            while(nums[i]!=0){
+                rem= nums[i]%10;
+                nums[i] /=10;
+                count++;
+            }
+            if(count %2==0){
+                c++;
+            }
+
+        }
+
+        return c;
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums ={12,345,2,6,7896};
+
+        System.out.println(findNumbers(nums));
     }
 
 }
