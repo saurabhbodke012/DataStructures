67853d5f2780d9991c8f592c619423f587c16ebe
Saurabh Bodke <saurabhbodke@Saurabhs-MacBook-Pro.local>
2021-11-26 16:57:08 -0500

    RemoveElement

--- DSA/src/Arrays/Insert.java
+++ DSA/src/Arrays/Insert.java
@@ -7,6 +7,9 @@ public class Insert {
     public static void duplicateZeros(int[] arr) {
         int n= arr.length;
         for(int i=0; i<n; i++){
+            if(i == n-1){
+                break;
+            }
             if(arr[i] == 0){
                 for(int j= (n-2); j>=(i+1); j--){
                     arr[j+1] = arr[j];
@@ -18,10 +21,33 @@ public class Insert {
         System.out.println(Arrays.toString(arr));
     }
 
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int nums1Index = 0;
+        int nums2Index = 0;
+        int[] merged = new int[m +n ];
+        while(nums1Index < nums1.length && nums2Index < nums2.length) {
+            if(nums1[nums1Index] < nums2[nums2Index]) {
+                merged[nums1Index + nums2Index] = nums1[nums1Index];
+                nums1Index++;
+            } else {
+                merged[nums1Index + nums2Index] = nums2[nums2Index];
+                nums2Index++;
+            }
+        }
+
+        for (int i=0; i< merged.length; i++){
+            nums1[i] = merged[i];
+        }
+    }
+
 
     public static void main(String[] args) {
         int[] arr= {1,0,2,3,0,4,5,0};
+        int[] nums1= {1,2,3,0,0,0};
+        int[] nums2={2,5,6};
 
-        duplicateZeros(arr);
+        merge(nums1,3,nums2,3);
+
+//        duplicateZeros(arr);
     }
 }
