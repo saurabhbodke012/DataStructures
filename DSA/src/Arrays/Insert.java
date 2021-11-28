e205aa7069fd0ad1105b25e3af4ac3aa42ef5715
Saurabh Bodke <saurabhbodke@Saurabhs-MacBook-Pro.local>
2021-11-27 19:50:18 -0500

    DS1

--- DSA/src/Arrays/Insert.java
+++ DSA/src/Arrays/Insert.java
@@ -21,24 +21,24 @@ public class Insert {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int nums1Index = 0;
-        int nums2Index = 0;
-        int[] merged = new int[m +n ];
-        while(nums1Index < nums1.length && nums2Index < nums2.length) {
-            if(nums1[nums1Index] < nums2[nums2Index]) {
-                merged[nums1Index + nums2Index] = nums1[nums1Index];
-                nums1Index++;
-            } else {
-                merged[nums1Index + nums2Index] = nums2[nums2Index];
-                nums2Index++;
-            }
-        }
-
-        for (int i=0; i< merged.length; i++){
-            nums1[i] = merged[i];
-        }
-    }
+//    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//        int nums1Index = 0;
+//        int nums2Index = 0;
+//        int[] merged = new int[m +n ];
+//        while(nums1Index < nums1.length && nums2Index < nums2.length) {
+//            if(nums1[nums1Index] < nums2[nums2Index]) {
+//                merged[nums1Index + nums2Index] = nums1[nums1Index];
+//                nums1Index++;
+//            } else {
+//                merged[nums1Index + nums2Index] = nums2[nums2Index];
+//                nums2Index++;
+//            }
+//        }
+//
+//        for (int i=0; i< merged.length; i++){
+//            nums1[i] = merged[i];
+//        }
+//    }
 
 
     public static void main(String[] args) {
@@ -46,7 +46,7 @@ public class Insert {
         int[] nums1= {1,2,3,0,0,0};
         int[] nums2={2,5,6};
 
-        merge(nums1,3,nums2,3);
+//        merge(nums1,3,nums2,3);
 
 //        duplicateZeros(arr);
     }
---
+++ DSA/src/DataStructure_I/DayTwo.java
@@ -0,0 +1,24 @@
+package DataStructure_I;
+
+import java.util.HashMap;
+
+public class DayTwo {
+    public int[] twoSum(int[] nums, int target){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int[] res = new int[2];
+
+        for(int i=0; i<nums.length; i++){
+
+            if(map.containsKey(target-nums[i])){
+                res[0] = i;
+                res[1] = map.get(target-nums[i]);
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return res;
+
+    }
+}
