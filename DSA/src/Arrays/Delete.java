6e084ac1cb9d92163c8adfc53d40f3090cc98c01
Saurabh Bodke <saurabhbodke@Saurabhs-MacBook-Pro.local>
2021-11-27 20:03:09 -0500

    DayTwo DS1

--- DSA/src/Arrays/Delete.java
+++ DSA/src/Arrays/Delete.java
@@ -18,9 +18,7 @@ public class Delete {
     public int removeDuplicates(int[] nums) {
         Arrays.sort(nums);
         int j=0;
-
         for(int i=0; i<nums.length; i++){
-
             if(nums[i] != nums[j]){
                 j++;
                 nums[j] = nums[i];
--- DSA/src/DataStructure_I/DayTwo.java
+++ DSA/src/DataStructure_I/DayTwo.java
@@ -1,5 +1,6 @@
 package DataStructure_I;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class DayTwo {
@@ -18,6 +19,40 @@ public class DayTwo {
             map.put(nums[i], i);
         }
         return res;
-
     }
+
+//    public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int nums1Index = 0;
+//         int nums2Index = 0;
+//         for(int i=m;i<nums1.length;i++){
+//             nums1[i]=nums2[nums2Index];
+//             nums2Index++;
+//         }
+//         for(int i=0; i < nums1.length; i++) {
+//             for(int index=i; index > 0 && nums1[index] < nums1[index-1]; index-- ) {
+//                 int temp = nums1[index];
+//                 nums1[index] = nums1[index-1];
+//                 nums1[index-1] = temp;
+
+//             }
+//         }
+//     }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index=0;
+        for (int i=m; i<(m+n); i++){
+            nums1[i] = nums2[index];
+            index++;
+        }
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+
+        merge(nums1, 3,nums2,3);
+    }
+
 }
