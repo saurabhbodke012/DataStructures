0d4982df3a43ee62ed776d9cf0d8cae3a48c4d7f
Saurabh Bodke <saurabhbodke@Saurabhs-MacBook-Pro.local>
2021-12-22 17:00:33 -0500

    SecondLargest

--- DSA/src/DataStructure_I/DayFour.java
+++ DSA/src/DataStructure_I/DayFour.java
@@ -15,8 +15,28 @@ public class DayFour {
 
     }
 
+    public static int secondLargestElement(int[] arr){
+        int largest = arr[0];
+        int secLargest= -1;
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] > largest) {
+                secLargest = largest;
+                largest = arr[i];
+            }else if(arr[i] != largest){
+                if(arr[i] > secLargest || secLargest == -1){
+                    secLargest = arr[i];
+                }
+            }
+        }
+
+        return secLargest;
+
+    }
+
+
+
     public static void main(String[] args) {
         int[] arr= {1,5,2,6,7,10,5,3};
-        System.out.println(largestElement(arr));
+        System.out.println(secondLargestElement(arr));
     }
 }
