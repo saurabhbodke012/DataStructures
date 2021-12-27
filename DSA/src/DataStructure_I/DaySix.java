378d83eb052b62af955ff214c88ef34322cb9af9
Saurabh Bodke <saurabhbodke@Saurabhs-MacBook-Pro.local>
2021-12-27 17:27:20 -0500

    day8

--- DSA/src/DataStructure_I/DaySix.java
+++ DSA/src/DataStructure_I/DaySix.java
@@ -20,9 +20,29 @@ public class DaySix {
         return finalCount;
     }
 
-    public static void main(String[] args) {
-        int[] arr={1,0,1,1,1,1,0,0,1,1};
+    public static int longestEvenSub(int[] arr){
+        int count=1;
+        int finalCount=0;
 
-        System.out.println(maxConsecutiveOnes(arr));
+        int index=0;
+        for(int i=1; i<arr.length; i++){
+
+            if( (arr[index] % 2==0 && arr[i] %2 !=0) || (arr[index] % 2 !=0 && arr[i] %2 ==0) ){
+                count++;
+            }else{
+                count =1;
+            }
+
+            index++;
+            finalCount = Math.max(finalCount,count);
+        }
+
+        return finalCount;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr={5,10,20,6,3,8};
+        System.out.println(longestEvenSub(arr));
     }
 }
