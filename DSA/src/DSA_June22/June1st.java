1c2d8e9ad6e74aee1b161ef29e8a333bff2d36ad
Saurabh Bodke <saurabhbodke@Saurabhs-MacBook-Pro.local>
2022-06-01 16:18:24 -0400

    1st June Problem 3

--- DSA/src/DSA_June22/June1st.java
+++ DSA/src/DSA_June22/June1st.java
@@ -16,15 +16,24 @@ public class June1st {
         if(n==0){
             return;
         }
-
         OnetoN(n-1);
         System.out.println(n);
+    }
+
+    public static int factorial(int n){
+
+        if(n==0 || n==1){
+            return 1;
+        }
+
+        return n*factorial(n-1);
     }
 
 
     public static void main(String[] args) {
 
 //        NtoOne(5);
-        OnetoN(5);
+//        OnetoN(5);
+        System.out.println(factorial(3));
     }
 }
