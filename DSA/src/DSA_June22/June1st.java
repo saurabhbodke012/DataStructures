bd36c3fb3ccc89176773d3bf29067cb0dbe562a7
Saurabh Bodke <saurabhbodke@Saurabhs-MacBook-Pro.local>
2022-06-01 17:03:23 -0400

    1st June Problem 5

--- DSA/src/DSA_June22/June1st.java
+++ DSA/src/DSA_June22/June1st.java
@@ -35,12 +35,32 @@ public class June1st {
         return fibonacci(n-1)+fibonacci(n-2);
     }
 
+    public static int getSum(int n){
+        if(n==0){
+            return 0;
+        }
+        return n + getSum(n-1);
+    }
+
+    public static boolean palindrome(String inp, int start, int end){
+
+        if(start>=end){
+            return true;
+        }
+
+        boolean b = ( inp.charAt(start) == inp.charAt(end) ) && palindrome(inp, start + 1, end - 1);
+        return b;
+    }
+
 
     public static void main(String[] args) {
 
 //        NtoOne(5);
 //        OnetoN(5);
 //        System.out.println(factorial(3));
-        System.out.println(fibonacci(3));
+//        System.out.println(fibonacci(3));
+//        System.out.println(getSum(2));
+//        System.out.println(palindrome("abba",0,3));
     }
+
 }
