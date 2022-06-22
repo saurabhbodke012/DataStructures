e104550a62a374e679fb6ed8254ae6e115e9e070
Saurabh Bodke <saurabhbodke@Saurabhs-MacBook-Pro.local>
2022-06-22 17:47:58 -0400

    climbing stairs memoization problem 2

--- DSA/src/DSA_June22/June22nd.java
+++ DSA/src/DSA_June22/June22nd.java
@@ -4,12 +4,12 @@ import java.util.HashMap;
 
 public class June22nd {
 
-    HashMap<Integer, Integer> cache = new HashMap<Integer, Integer>();
+    HashMap<Integer, Integer> cache1 = new HashMap<Integer, Integer>();
 
     public int fib(int n) {
 
-        if(cache.containsKey(n)){
-            return cache.get(n);
+        if(cache1.containsKey(n)){
+            return cache1.get(n);
         }
 
         int res=0;
@@ -20,7 +20,28 @@ public class June22nd {
             res= fib(n-1) + fib(n-2);
         }
 
-        cache.put(n, res);
+        cache1.put(n, res);
+        return res;
+
+    }
+
+    HashMap<Integer, Integer> cache2 = new HashMap<Integer, Integer>();
+
+    public int climbStairs(int n) {
+
+        if(cache2.containsKey(n)){
+            return cache2.get(n);
+        }
+
+        int res=0;
+
+        if(n<4){
+            res=n;
+        }else{
+            res= climbStairs(n-1)+ climbStairs(n-2);
+        }
+
+        cache2.put(n,res);
         return res;
 
     }
