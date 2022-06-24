5f3b66a2b549b263c4d016ff6550ed688e1b3c50
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-06-23 20:23:11 -0400

    day 2 dsa-1

---
+++ DSA/src/DataStructure_1/Day2.java
@@ -0,0 +1,26 @@
+package DataStructure_1;
+
+public class Day2 {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int index1 = m - 1;
+        int index2 = n - 1;
+
+
+        for (int i = nums1.length-1; i >= 0; i--) {
+            if (index2 < 0) {
+                break;
+            }
+            if (index1 >= 0 && nums1[index1] > nums2[index2]) {
+                nums1[i] = nums1[index1];
+                index1--;
+            } else {
+                nums1[i] = nums2[index2];
+                index2--;
+            }
+        }
+    }
+
+
+}
